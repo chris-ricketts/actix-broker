455fb74830a39a11b6993a852a959f203226a921
chris ricketts <chris.ricketts@steribar.com>
2018-09-15 02:10:14 +0100

    trait doc comments

--- src/issue.rs
+++ src/issue.rs
@@ -3,7 +3,12 @@ use actix::prelude::*;
 use broker::Broker;
 use msgs::*;
 
+/// The `BrokerIssue` provides functions to issue messages to any subscribers.
 pub trait BrokerIssue: Actor<Context = Context<Self>> {
+    /// Asynchronously issue a message.
+    /// ```
+    /// self.issue_async(MessageType);
+    /// ```
     fn issue_async<M: BrokerMsg>(&self, msg: M)
     where
         <M as Message>::Result: Send,
@@ -12,6 +17,12 @@ pub trait BrokerIssue: Actor<Context = Context<Self>> {
         broker.do_send(IssueAsync(msg));
     }
 
+    /// Synchronously issue a message.
+    /// ```
+    /// self.issue_sync(MessageType, ctx);
+    /// ```
+    /// This also causes the broker to synchronously forward those messages on to any subscribers
+    /// before handling any other messages.
     fn issue_sync<M: BrokerMsg>(&self, msg: M, ctx: &mut Self::Context)
     where
         <M as Message>::Result: Send,
--- src/subscribe.rs
+++ src/subscribe.rs
@@ -1,9 +1,16 @@
+//! messages.
 use actix::prelude::*;
 
 use broker::Broker;
 use msgs::*;
 
+/// The `BrokerSubscribe` trait has functions to register an actor's interest in different
+/// messages.
 pub trait BrokerSubscribe: Actor<Context = Context<Self>> {
+    /// Asynchronously subscribe to a message.
+    /// ```
+    /// self.subscribe_async::<MessageType>(ctx);
+    /// ```
     fn subscribe_async<M: BrokerMsg>(&self, ctx: &mut Self::Context)
     where
         <M as Message>::Result: Send,
@@ -14,6 +21,13 @@ pub trait BrokerSubscribe: Actor<Context = Context<Self>> {
         broker.do_send(SubscribeAsync(recipient));
     }
 
+    /// Synchronously subscribe to a message.
+    /// ```
+    /// self.subscribe_sync::<MessageType>(ctx);
+    /// ```
+    /// This actor will do nothing else until its interest is registered.
+    /// If messages of that type have been sent to the broker previously, a copy of the latest
+    /// message is sent to the calling actor after it has subscribed.
     fn subscribe_sync<M: BrokerMsg>(&self, ctx: &mut Self::Context)
     where
         <M as Message>::Result: Send,
